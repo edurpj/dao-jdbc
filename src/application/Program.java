5291059a9074f1d3311fcb1bc03fcf73ab4da5d5
Eduardo <edurpj2@gmail.com>
2021-09-01 17:21:25 -0300

    Seller class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,22 @@
 package application;
 
+import com.mysql.fabric.xmlrpc.base.Data;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		
+		Seller seller = new Seller();
+		//seller.setBaseSalary(3000);
+		seller.setName("Teste");
 		Department obj = new Department(1, "Books");
 		System.out.println(obj);
+		
+		System.out.println(seller);
 
 	}
 

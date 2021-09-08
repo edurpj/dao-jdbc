718b20412c45cc839c58bf0a545db8a619e2c882
Eduardo <edurpj2@gmail.com>
2021-09-08 10:08:47 -0300

    Find All

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,24 +10,31 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-		
-		
-		//Seller seller = new Seller();
-		//seller.setBaseSalary(3000);
-		//seller.setName("Teste");
-		
+
+		// Seller seller = new Seller();
+		// seller.setBaseSalary(3000);
+		// seller.setName("Teste");
+
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
+
 		System.out.println("________ TEST 1: find By id: ________ ");
-		Seller seller = sellerDao.findById(3);	
+		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
-		
+
 		System.out.println("________ TEST 2: find By department: ________ ");
-		
+
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
-		
-		for(Seller obj : list) {
+
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
+
+		System.out.println("________ TEST 3: find all: ________ ");
+
+		list = sellerDao.findAll();
+
+		for (Seller obj : list) {
 			System.out.println(obj);
 		}
 
